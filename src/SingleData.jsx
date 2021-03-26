12254573923a306bcd2b1dc520e54c92806c8f7a
david <er.daviddabwali@gmail.com>
2021-03-26 23:30:04 +0530

    without consumer and use contextype

--- src/SingleData.jsx
+++ src/SingleData.jsx
@@ -1,60 +1,46 @@
 import React from "react"
 import Name from "./Name"
 import { UserContext } from "./contextApi"
-
+ 
 export default class SingleData extends React.Component{
+    
+    static contextType = UserContext;
 
-     getData=(contextData)=>{
+     getData=(context)=>{
 
          //console.log(this.props.data)
          if(this.props.data !==undefined){
-            contextData.handler(this.props.data)
+            context.handler(this.props.data)
          }
      }
     
-
-    
-
     
     render(){
 
         
         let data=null
 
+        if(this.props.select!==undefined && this.props.data[this.props.select]){
+            data=<div style={{border:this.props.border}} className="leftData">
+               <Name singleName={this.props.data[this.props.select]}/>
+            </div>
+           }
+         else{
+            data=<div>
+            <h1>UserDetail is Below:</h1>
+            <h1>Name: {this.context.selectedData.name}</h1>
+            <h2>City: {this.context.selectedData.address.city}</h2>
+            <h2>Phone: {this.context.selectedData.phone}</h2>
+        </div>
+         }
+
         return(
-            <UserContext.Consumer>
-                
-                {
-                    (contextData)=>{
-                        if(this.props.select!==undefined && this.props.data[this.props.select]){
-                            data=<div style={{border:this.props.border}} className="leftData">
-                               <Name singleName={this.props.data[this.props.select]}/>
-                            </div>
-                           }
-                         else{
-                            data=<div>
-                            <h1>UserDetail is Below:</h1>
-                            <h1>Name: {contextData.selectedData.name}</h1>
-                            <h2>City: {contextData.selectedData.address.city}</h2>
-                            <h2>Phone: {contextData.selectedData.phone}</h2>
-                        </div>
-                         }
-                         return(
-                             <div onClick={()=>{this.getData(contextData)}} className="detail">
-                             {data}
-                             </div>
-                         )  
-                    }          
-                }
-                
-       </UserContext.Consumer>
-            
-            
+            <div onClick={this.getData.bind(this,this.context)} className="detail"> 
+            {/* this.context is second parameter but act as first */}
+            {data}
+            </div>
+       
         )
     }
-
-
-
-
 
 }
